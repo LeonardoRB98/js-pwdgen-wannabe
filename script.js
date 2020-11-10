f15124df21450cdca723a95fe1ccf02fa63d9597
Leonardo <leonardo.ranaldo.b@gmail.com>
2020-11-10 15:25:47 +0100

    primo commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var nome = prompt('Come ti chiami?');
+console.log(nome);
+var cognome = prompt('Quale è il tuo cognome?');
+console.log(cognome);
+var colore = prompt('Quale è il tuo colore preferito?');
+console.log(colore);
+var password = nome + cognome +  colore;
+console.log(password)
+document.getElementById('password').innerHTML = password;
